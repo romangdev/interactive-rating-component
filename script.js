f0e9a080a274e29599bc397f8610b6efe33c60cf
Roman Geathers <romangeathersdev@gmail.com>
2022-06-07 19:36:27 -0400

    Change btn color to orange on click

---
+++ script.js
@@ -0,0 +1,8 @@
+let ratingButtons = document.querySelectorAll(".rating button");
+
+ratingButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.classList.remove("button-grey");
+    button.classList.add("button-orange");
+  });
+});
